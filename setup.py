c92eb8e2099322bff15d010d0cc461f8015305a3
SankalpMakol016 <sankalp.sumit.makol@gmail.com>
2026-05-30 12:34:26 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirments(file_path:str)->List[str]:
+    requirments = []
+    with open(file_path) as file_obj:
+        requirments = file_obj.readlines()
+        requirments=[rec.replace('/n','') for rec in requirments]
+        
+        if '-e .' in requirments:
+            requirments.remove('-e .')
+    return requirments
+        
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='sankalp',
+    author_email='sankalp.makol16@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirments('requirments.txt')
+)
