c68a6411459bbb4216a781d654e96ae1d8b01b65
SankalpMakol016 <sankalp.sumit.makol@gmail.com>
2026-06-01 13:50:07 +0530

    Implement data transformation pipeline and object serialization

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class dataingestionconfig:
     train_data_path: str = os.path.join("artifacts","train.csv")
@@ -49,5 +52,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_set , test_set = obj.initiate_data_ingestion()
+    
+    datatransformation = DataTransformation()
+    datatransformation.initiate_data_transformation(train_set,test_set)
     
